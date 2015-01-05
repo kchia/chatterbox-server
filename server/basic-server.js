fd5c321ba6cff9c9a9a52fd30020779b932b4b38
Luke Davis <lukefable@gmail.com>
2015-01-05 20:13:54 +0000

    i think i got the initial server running - notable is the requirement that i default the port and ip to c9's env port and IP so the built-in architecture can detect it.

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,18 +1,18 @@
 /* Import node's http module: */
 var http = require("http");
-
+var handleRequest=require("./request-handler.js").requestHandler;
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
 // normally already claimed by another server and/or not accessible
 // so we'll use a standard testing port like 3000, other common development
 // ports are 8080 and 1337.
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 // For now, since you're running this server on your local machine,
 // we'll have it listen on the IP address 127.0.0.1, which is a
 // special address that always refers to localhost.
-var ip = "127.0.0.1";
+var ip =process.env.IP || "127.0.0.1";
 
 
 
@@ -24,7 +24,8 @@ var ip = "127.0.0.1";
 // After creating the server, we will tell it to listen on the given port and IP. */
 var server = http.createServer(handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+//i have to do the process.env.PORT and IP thing because of c9
+server.listen(port,ip);
 
 // To start this server, run:
 //
@@ -38,4 +39,3 @@ server.listen(port, ip);
 // server.listen() will continue running as long as there is the
 // possibility of serving more requests. To stop your server, hit
 // Ctrl-C on the command line.
-
